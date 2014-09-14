bcfcdb8426502df11031e391e7567fe3806a8f78
Nicolas Bonnici <nicolasbonnici@gmail.com>
2014-09-15 01:35:10 +0200

    "Auto generated commit message for user bundle by sociableCore framework console app, see diff for more infos"

---
+++ Entities/Mapping/Collection/UserGroupCollection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace bundles\user\Entities\Mapping\Collection;
+
+/**
+ * UserGroup collection
+ *
+ * @author infradmin
+ */
+class UserGroupCollection extends \Library\Core\EntitiesCollection {}
---
+++ Entities/Mapping/UserGroup.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace bundles\user\Entities\Mapping;
+
+/**
+ * \app\Entity\UserGroup Entity
+ *
+ * @see \app\Entities\User
+ * @author infradmin
+ */
+class UserGroup extends \Library\Core\Entity {
+
+    const ENTITY = 'UserGroup';
+    const TABLE_NAME = 'userGroups';
+    const PRIMARY_KEY = 'idusergroup';
+
+    /**
+     * Object caching duration in seconds
+     * @var integer
+     */
+    protected $iCacheDuration = 120;
+
+    protected $aLinkedEntities;
+
+}
